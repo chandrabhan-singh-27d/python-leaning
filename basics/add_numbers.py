e8e48c91423fa675822d389cf1b6e1e2ae843493
chandrabhan <chandrabhansingh27d@gmail.com>
2024-04-09 20:10:30 +0530

    basics

--- basics/add_numbers.py
+++ basics/add_numbers.py
@@ -1,2 +1,10 @@
 def add_numbers(a,b):
     return a + b
+
+def subtract_numbers(a,b):
+    # proper conditionals
+    if a > b:
+        return a-b 
+    else: 
+        return b-a
+    
--- basics/first.py
+++ basics/first.py
@@ -1,5 +1,6 @@
-from add_numbers import add_numbers
+from add_numbers import add_numbers, subtract_numbers
 
 print('Hello World!')
 
 print("sum of numbers is:", add_numbers(2,3))
+print("difference of numbers is:", subtract_numbers(2,3))
